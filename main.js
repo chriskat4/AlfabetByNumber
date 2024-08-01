6a303b73dfbc847746043bec796d9824f4c9b154
chris <christianpimpo304@hotmail.com>
2024-08-01 11:42:14 -0300

    tests

--- main.js
+++ main.js
@@ -3,5 +3,19 @@ const Alfabet = require('./Alfabet.js');
 const alfabeto = new Alfabet();
 
 let TEST = alfabeto.especNumberAlfabet(4);
+console.log(TEST);
 
+TEST = alfabeto.especNumberAlfabet(7);
+console.log(TEST);
+
+TEST = alfabeto.especNumberAlfabet(8);
+console.log(TEST);
+
+TEST = alfabeto.especNumberAlfabet(0);
+console.log(TEST);
+
+TEST = alfabeto.especNumberAlfabet(27);
+console.log(TEST);
+
+TEST = alfabeto.especNumberAlfabet(4);
 console.log(TEST);
